a0c886c5b6c7eeaefff8a7b13f833a2bdbb9ed41
KatySFreel <97260406+KatySFreel@users.noreply.github.com>
2022-07-18 15:37:48 +0300

    finish

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var slider1 = sliderFactory.createSlider();
-var slider2 = sliderFactory.createSlider();
+var slider1 = new Slider();
+var slider2 = new Slider();
 
 slider1.start('slider1');
 slider2.start('slider2');
